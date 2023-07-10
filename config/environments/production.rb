1808cadb4299a6688dab75d359d77df22d1d36fc
Prajay Chaudhary <prajaychaudhary5678@gmail.com>
2023-07-10 15:34:30 +0545

    added host to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,4 +105,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+    config.hosts = [
+    "aid-backend-api-production.up.railway.app"
+  ]
 end
